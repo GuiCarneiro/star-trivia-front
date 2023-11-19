6e6c4c401f3712fb81f4e2b8eac3e979e5da5b49
GuiCarneiro <gui.carneiro15@gmail.com>
2023-11-19 02:03:08 -0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,45 @@
-/* Components */
-import { Counter } from './components/Counter/Counter'
+import styles from './styles/index.module.scss';
+import Link from 'next/link';
 
-export default function IndexPage() {
-  return <Counter />
+function IndexPage() {
+  return (
+    <div className={styles.mainMenu}>
+      <Link
+        href="/classic"
+        className={styles.menuLink}
+      > 
+        <div className={styles.menuButton}>
+          <div className={styles.menuTitle}>
+            Classic
+          </div>
+
+          <div className={styles.menuDescription}>
+            Get clues and guess a character
+          </div>
+        </div>
+      </Link>
+
+      <Link
+        href="/quotes"
+        className={styles.menuLink}
+      > 
+        <div className={styles.menuButton}>
+          <div className={styles.menuTitle}>
+            Quotes
+          </div>
+
+          <div className={styles.menuDescription}>
+            Guess with in-game quotes
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export const metadata = {
   title: 'Redux Toolkit',
 }
+
+
+export default IndexPage
