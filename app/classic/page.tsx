56e4a1d09865efdede04a3bbb2000db7981db1d8
GuiCarneiro <gui.carneiro15@gmail.com>
2023-11-19 02:26:54 -0300

    addin classic and quote pages

---
+++ app/classic/page.tsx
@@ -0,0 +1,16 @@
+import styles from './styles/index.module.scss';
+
+function ClassicPage() {
+  return (
+    <div>
+      CLASSIC!
+    </div>
+  )
+}
+
+export const metadata = {
+  title: 'Redux Toolkit',
+}
+
+
+export default ClassicPage
---
+++ app/quotes/page.tsx
@@ -0,0 +1,16 @@
+import styles from './styles/index.module.scss';
+
+function QuotesPage() {
+  return (
+    <div>
+      CLASSIC!
+    </div>
+  )
+}
+
+export const metadata = {
+  title: 'Redux Toolkit',
+}
+
+
+export default QuotesPage
